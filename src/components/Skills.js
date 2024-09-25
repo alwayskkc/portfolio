2e67892ba1c5051f5c6e3da51079d19abbc14495
Kesava <palvadi.krish@gmail.com>
2024-09-24 21:48:02 -0400

    Added PNG images in Skills.js file

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,15 @@
 import React from "react";
-import cIcon from "../assets/images/C.svg"; // Importing the C icon in PNG format
-import csharpIcon from "../assets/images/C#.svg"; // Importing the C# icon in PNG format
-import javaIcon from "../assets/images/Java.svg"; // Importing the Java icon in PNG format
-import pythonIcon from "../assets/images/Python.svg"; // Importing the Python icon in PNG format
-import awsIcon from "../assets/images/AWS.svg"; // Importing the AWS icon in PNG format
-import azureIcon from "../assets/images/Azure.svg"; // Importing the Azure icon in PNG format
-import reactIcon from "../assets/images/ReactJS.svg"; // Importing the ReactJS icon in PNG format
+import cIcon from "../assets/images/C.png"; // Importing the C icon in PNG format
+import csharpIcon from "../assets/images/Csharp.png"; // Importing the C# icon in PNG format
+import javaIcon from "../assets/images/Java.png"; // Importing the Java icon in PNG format
+import pythonIcon from "../assets/images/Python.png"; // Importing the Python icon in PNG format
+import awsIcon from "../assets/images/aws.png"; // Importing the AWS icon in PNG format
+import azureIcon from "../assets/images/azure.png"; // Importing the Azure icon in PNG format
+import reactIcon from "../assets/images/reactjs.png"; // Importing the ReactJS icon in PNG format
+import cplusIcon from "../assets/images/C++.png";
+import sqlIcon from "../assets/images/sql.png";
+import aspIcon from "../assets/images/asp.png";
+import jsIcon from "../assets/images/js.png";
 
 function Skills() {
   return (
@@ -38,6 +42,22 @@ function Skills() {
         </div>
         <div className="skill-item">
           <img src={reactIcon} alt="ReactJS" />
+          <p>ReactJS</p>
+        </div>
+        <div className="skill-item">
+          <img src={cplusIcon} alt="C++" />
+          <p>ReactJS</p>
+        </div>
+        <div className="skill-item">
+          <img src={aspIcon} alt="ASP .NET" />
+          <p>ReactJS</p>
+        </div>
+        <div className="skill-item">
+          <img src={sqlIcon} alt="SQL" />
+          <p>ReactJS</p>
+        </div>
+        <div className="skill-item">
+          <img src={jsIcon} alt="Java Script" />
           <p>ReactJS</p>
         </div>
       </div>
