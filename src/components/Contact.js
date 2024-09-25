5fef04f7f6b82d00babf59758570055c3fe057da
alwayskkc <61517325+alwayskkc@users.noreply.github.com>
2024-09-24 22:02:20 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,7 +5,8 @@ function Contact() {
     <section id="contact" className="section">
       <h2>Contact Me</h2>
       <p>Email: <a href="mailto:kkc.palvadi@gmail.com">kkc.palvadi@gmail.com</a></p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/kkcpalvadi" target="_blank">linkedin.com/in/kkcpalvadi</a></p>
+      <p>LinkedIn: <a href="https://www.linkedin.com/in/kkcpalvadi" target="_blank" rel="noopener noreferrer"> LinkedIn Profile </a></p>
+
     </section>
   );
 }
