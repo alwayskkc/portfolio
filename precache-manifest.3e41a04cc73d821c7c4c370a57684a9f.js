0c7b26ca80db8d097c65a31f3ff672bb644b98fa
Kesava <palvadi.krish@gmail.com>
2024-09-24 22:12:12 -0400

    Updates

---
+++ precache-manifest.3e41a04cc73d821c7c4c370a57684a9f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9150d28db1b53019682ef47d8ceb9fce",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b32aa2afb6479a27f25e",
+    "url": "/portfolio/static/css/main.597366f5.chunk.css"
+  },
+  {
+    "revision": "975a10caecdfe1b3380a",
+    "url": "/portfolio/static/js/2.9dea8caa.chunk.js"
+  },
+  {
+    "revision": "b32aa2afb6479a27f25e",
+    "url": "/portfolio/static/js/main.bb61b1e4.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "633bb28c1e01b0f1a13fccf6950a4c41",
+    "url": "/portfolio/static/media/C++.633bb28c.png"
+  },
+  {
+    "revision": "ad579afd54a95d6e231aaac136f10c75",
+    "url": "/portfolio/static/media/C.ad579afd.png"
+  },
+  {
+    "revision": "909c17a68461df319339ae456d61e016",
+    "url": "/portfolio/static/media/Csharp.909c17a6.png"
+  },
+  {
+    "revision": "45b251358f56b647691ecd7cd4f3c241",
+    "url": "/portfolio/static/media/Java.45b25135.png"
+  },
+  {
+    "revision": "f24bb490085a41826fe051d56e8617ba",
+    "url": "/portfolio/static/media/Python.f24bb490.png"
+  },
+  {
+    "revision": "a8313b92fc9b57b2be50ffe8fcf69111",
+    "url": "/portfolio/static/media/asp.a8313b92.png"
+  },
+  {
+    "revision": "9d0493437c0b9cd5bb3fd26b2145c378",
+    "url": "/portfolio/static/media/aws.9d049343.png"
+  },
+  {
+    "revision": "388883d78b925c9ec3d0aec807a911ae",
+    "url": "/portfolio/static/media/azure.388883d7.png"
+  },
+  {
+    "revision": "2d2b0d56aeefb1b0f3025195cb2ff7b6",
+    "url": "/portfolio/static/media/reactjs.2d2b0d56.png"
+  },
+  {
+    "revision": "077f952b2eda4a50e57c41aa211df007",
+    "url": "/portfolio/static/media/sql.077f952b.png"
+  },
+  {
+    "revision": "1e64a9ffe9f04ca2a325e4fe6b9eb7e4",
+    "url": "/portfolio/static/media/your-photo.1e64a9ff.jpg"
+  }
+]);
