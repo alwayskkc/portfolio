96402a1b2a09f76003c126a09e5599098a53b6b2
alwayskkc <61517325+alwayskkc@users.noreply.github.com>
2024-09-24 22:24:34 -0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,9 +4,12 @@ function Contact() {
   return (
     <section id="contact" className="section">
       <h2>Contact Me</h2>
-      <p>Email: <a href="mailto:kkc.palvadi@gmail.com">kkc.palvadi@gmail.com</a></p>
-      <p>LinkedIn: <a href="https://www.linkedin.com/in/kkcpalvadi" target="_blank" rel="noopener noreferrer"> LinkedIn Profile </a></p>
-
+      <form className="contact-form">
+        <input type="text" placeholder="Your Name" />
+        <input type="email" placeholder="Your Email" />
+        <textarea rows="5" placeholder="Your Message"></textarea>
+        <button type="submit">Send Message</button>
+      </form>
     </section>
   );
 }
